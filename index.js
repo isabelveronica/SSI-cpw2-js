8a946f759c93507a901d8bb7e03d7d478f8273c0
isabelveronica <isabelvfr@gmail.com>
2022-12-16 08:47:25 -0300

    Resolução Atividade

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-
 /**
  * Recebe um array com os nomes de pessoas e retorna um array invertido
  *
  * @param {*} people : Um array contendo um conjunto de nomes
  * @returns Um array com os nomes invertidos
  */
-function invert(people){
-
-    // TODO
-    // 1) Peça ao usuário para digitar vários nomes. Exiba na tela
-    // todos os nomes digitados, porém de maneira invertida (do último para o primeiro).
-    // Uma dica, você pode utilizar a função prompt para permitir que o usuário digite os
-    // nomes dos usuários.
-
-    return [];
+function invert(people) {
+  // TODO
+  // 1) Peça ao usuário para digitar vários nomes. Exiba na tela
+  // todos os nomes digitados, porém de maneira invertida (do último para o primeiro).
+  // Uma dica, você pode utilizar a função prompt para permitir que o usuário digite os
+  // nomes dos usuários.
+  let invertPeople = [];
+  for (let i = people.length - 1; i >= 0; i--) {
+    invertPeople.push(people[i]);
+  }
+  return invertPeople;
 }
 
 /**
@@ -22,14 +23,17 @@ function invert(people){
  * @param {*} grades : Um array de notas
  * @returns Uma média a partir do array de notas
  */
-function mean(grades){
+function mean(grades) {
+  // TODO
+  // 2) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
+  // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
+  // ou superior a 7.
+  let n1 = grades[0];
+  let n2 = grades[1];
+  let n3 = grades[2];
+  let media = (n1 + n2 + n3) / 3;
 
-    // TODO
-    // 2) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
-    // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
-    // ou superior a 7.
-
-    return 0;
+  return media;
 }
 
 /**
@@ -38,14 +42,16 @@ function mean(grades){
  * @param {*} mean : Um número que representa uma média
  * @returns Uma string dizendo se um estudante está aprovado ou não
  */
-function isApproved(mean){
-
-    // TODO
-    // 2.1) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
-    // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
-    // ou superior a 7.
-
-    return "";
+function isApproved(mean) {
+  // TODO
+  // 2.1) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
+  // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
+  // ou superior a 7.
+  if (mean >= 7) {
+    return 'aprovado';
+  } else {
+    return 'reprovado';
+  }
 }
 
 /**
@@ -54,21 +60,67 @@ function isApproved(mean){
  * @param {*} strDate : Uma string no formato de data
  * @returns Uma descrição da data informada
  */
-function wide(strDate){
+function wide(strDate) {
+  // TODO
+  // 3) Faça um script que receba uma data no formato “dd/mm/aaaa” e escreva a data por extenso,
+  // por exemplo, de "03/03/2022" para "03 de março de 2022". Dica: use a função
+  // “split” de uma string que quebra a string em pedaços dado um separador como argumento da função.
+  // Nesse caso, o separador é a barra (/) da data.
+  let data = strDate.split('/');
+  let mes = data[1];
 
-    // TODO
-    // 3) Faça um script que receba uma data no formato “dd/mm/aaaa” e escreva a data por extenso,
-    // por exemplo, de "03/03/2022" para "03 de março de 2022". Dica: use a função
-    // “split” de uma string que quebra a string em pedaços dado um separador como argumento da função.
-    // Nesse caso, o separador é a barra (/) da data.
-
-    return "";
+  switch (mes) {
+    case '01':
+      mes = 'janeiro';
+      break;
+    case '02':
+      mes = 'fevereiro';
+      break;
+    case '02':
+      mes = 'março';
+      break;
+    case '04':
+      mes = abril;
+      break;
+    case '05':
+      mes = 'maio';
+      break;
+    case '06':
+      mes = 'junho';
+      break;
+    case '07':
+      mes = 'julho';
+      break;
+    case '08':
+      mes = 'agosto';
+      break;
+    case '09':
+      mes = 'setembro';
+      break;
+    case '10':
+      mes = 'outubro';
+      break;
+    case '11':
+      mes = 'novembro';
+      break;
+    case '12':
+      mes = 'dezembro';
+      break;
+    default:
+  }
+  if (strDate === '') {
+    return '';
+  } else if (strDate !== data[0] + '/' + data[1] + '/' + data[2]) {
+    return '';
+  } else {
+    return data[0] + ' de ' + mes + ' de ' + data[2];
+  }
 }
 
 // Trecho utilizado pelos testes
 exports._test = {
-    invert: invert,
-    mean: mean,
-    isApproved: isApproved,
-    wide: wide
-}
+  invert: invert,
+  mean: mean,
+  isApproved: isApproved,
+  wide: wide,
+};
